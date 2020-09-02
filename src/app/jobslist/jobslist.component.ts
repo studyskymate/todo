8da9f4404cbf305f7e02631d63d021ace23f5b5c
studyskymate <30974908+studyskymate@users.noreply.github.com>
2020-09-02 23:24:20 +1200

    working on jobs page

--- src/app/jobslist/jobslist.component.ts
+++ src/app/jobslist/jobslist.component.ts
@@ -1,11 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export class Job{
+  public id:BigInteger;
+  public title:String;
+  public logoUrl:String;
+  public type:String;
+  public companyName:String;
+  public companyAddress:String;
+  public salary:String;
+}
+
+
 @Component({
   selector: 'app-jobslist',
   templateUrl: './jobslist.component.html',
   styleUrls: ['./jobslist.component.css']
 })
 export class JobslistComponent implements OnInit {
+
+  job: Job = {
+     id:null,
+     title:'Software Engineer',
+     logoUrl:'logourl',
+     type:'Full Time.aa',
+     companyName:'Googless',
+     companyAddress:'Street',
+     salary:'$50000'
+};
+  
+
+
 
   constructor() { }
 
